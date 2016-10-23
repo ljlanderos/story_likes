b9678de992283e5f4c21141da5cab4d35d272705
Luis Landeros <Landeros@Luis-Landeross-MacBook-Pro.local>
2016-10-23 11:26:38 -0400

    Ruby Movie Excercise

--- story.rb
+++ story.rb
@@ -1,4 +1,14 @@
+	puts("Welcome to Movie Rating!")
+	puts("Enter a Movie Title")
+	movie_title = gets.strip
+	puts("Enter a Rating for #{movie_title} out of 10")
+	movie_rating = gets.strip
 
+def movie_info(movie_title, movie_rating)
+	puts ("Here's your input! #{movie_title} is rated #{movie_rating} according to you!")
+end
+
+puts(movie_info(movie_title, movie_rating))
 
 #write a function that accepts two parameters as strings: movie title and movie rating (out of ten)
   #the function, when called, should print the story title and the rating out of ten
@@ -14,4 +24,4 @@
 
 # store the input in a variable
 
-display_story(user_title, user_rating)
+
